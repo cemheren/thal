c980abb9f333f5d2431e536739a9d634a735dc86
Akif Herenakhe@microsoft.com <akhe@microsoft.com>
2018-01-26 19:21:11 -0800

    canges

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const puppeteer = require('puppeteer');
-const CREDS = require('./creds');
-const mongoose = require('mongoose');
-const User = require('./models/user');
+
+async function resizePage(browser, page){
+  var height = 1000;
+  var width = 1600;
+  await page.setViewport({height, width});
+  // Window frame - probably OS and WM dependent.
+  height += 85;
+  // Any tab.
+  const {targetInfos: [{targetId}]} = await browser._connection.send(
+    'Target.getTargets'
+  );
+  // Tab window. 
+  const {windowId} = await browser._connection.send(
+    'Browser.getWindowForTarget',
+    {targetId}
+  );
+  // Resize.
+  await browser._connection.send('Browser.setWindowBounds', {
+    bounds: {height, width},
+    windowId
+  })
+};
 
 async function run() {
   const browser = await puppeteer.launch({
@@ -9,117 +28,48 @@ async function run() {
   });
 
   const page = await browser.newPage();
+  page.on('console', msg => console.log('PAGE LOG: ', msg.text()));
+  page.on('pageerror', error => {
+    console.log('pageerror: ' + error.message());
+   });
+  page.on('response', response => {
+    console.log('response: ' + response.status() + response.url());
+  });
+  page.on('requestfailed', request => {
+    console.log('requestfailed: ' + request.failure().errorText + request.url());
+  });
 
-  // await page.goto('https://github.com');
-  // await page.screenshot path: 'screenshots/github.png' });
-
-  await page.goto('https://github.com/login');
+  await resizePage(browser, page);
 
   // dom element selectors
-  const USERNAME_SELECTOR = '#login_field';
-  const PASSWORD_SELECTOR = '#password';
-  const BUTTON_SELECTOR = '#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block';
+  const USERNAME_SELECTOR = '#i0116';
+  const PASSWORD_SELECTOR = '#i0118';
+  const NEXTBUTTON_SELECTOR = '#idSIButton9';
+  
+  await page.goto('https://portal.azure.com/#blade/Microsoft_Azure_Policy/PolicyMenuBlade/Overview');
+  await page.waitFor(2*1000);
 
   await page.click(USERNAME_SELECTOR);
-  await page.keyboard.type(CREDS.username);
+  await page.keyboard.type("cemheren@gmail.com");
+
+  await page.waitFor(100);
+  
+  await page.click(NEXTBUTTON_SELECTOR);
+  await page.waitFor(3*1000);
 
   await page.click(PASSWORD_SELECTOR);
-  await page.keyboard.type(CREDS.password);
+  await page.keyboard.type("");
 
-  await page.click(BUTTON_SELECTOR);
-  await page.waitForNavigation();
+  await page.click(NEXTBUTTON_SELECTOR);
+  await page.waitFor(3*1000);
 
-  const userToSearch = 'john';
-  const searchUrl = `https://github.com/search?q=${userToSearch}&type=Users&utf8=%E2%9C%93`;
-  // let searchUrl = 'https://github.com/search?utf8=%E2%9C%93&q=bashua&type=Users';
+  await page.click(NEXTBUTTON_SELECTOR);
+  await page.waitFor(20*1000);
 
-  await page.goto(searchUrl);
-  await page.waitFor(2 * 1000);
+  // we are at the overview blade
+  await page.screenshot({ path: 'screenshots/overview.png' });
 
-  // const LIST_USERNAME_SELECTOR = '#user_search_results > div.user-list > div:nth-child(1) > div.d-flex > div > a';
-  const LIST_USERNAME_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > a';
-  // const LIST_EMAIL_SELECTOR = '#user_search_results > div.user-list > div:nth-child(1) > div.d-flex > div > ul > li:nth-child(2) > a';
-  const LIST_EMAIL_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > ul > li:nth-child(2) > a';
-  const LENGTH_SELECTOR_CLASS = 'user-list-item';
-  const numPages = await getNumPages(page);
-
-  console.log('Numpages: ', numPages);
-
-  for (let h = 1; h <= numPages; h++) {
-    let pageUrl = searchUrl + '&p=' + h;
-    await page.goto(pageUrl);
-
-    let listLength = await page.evaluate((sel) => {
-      return document.getElementsByClassName(sel).length;
-    }, LENGTH_SELECTOR_CLASS);
-
-    for (let i = 1; i <= listLength; i++) {
-      // change the index to the next child
-      let usernameSelector = LIST_USERNAME_SELECTOR.replace("INDEX", i);
-      let emailSelector = LIST_EMAIL_SELECTOR.replace("INDEX", i);
-
-      let username = await page.evaluate((sel) => {
-        return document.querySelector(sel).getAttribute('href').replace('/', '');
-      }, usernameSelector);
-
-      let email = await page.evaluate((sel) => {
-        let element = document.querySelector(sel);
-        return element ? element.innerHTML : null;
-      }, emailSelector);
-
-      // not all users have emails visible
-      if (!email)
-        continue;
-
-      console.log(username, ' -> ', email);
-
-      upsertUser({
-        username: username,
-        email: email,
-        dateCrawled: new Date()
-      });
-    }
-  }
-
-  browser.close();
-}
-
-async function getNumPages(page) {
-  const NUM_USER_SELECTOR = '#js-pjax-container > div.container > div > div.column.three-fourths.codesearch-results.pr-6 > div.d-flex.flex-justify-between.border-bottom.pb-3 > h3';
-
-  let inner = await page.evaluate((sel) => {
-    let html = document.querySelector(sel).innerHTML;
-
-    // format is: "69,803 users"
-    return html.replace(',', '').replace('users', '').trim();
-  }, NUM_USER_SELECTOR);
-
-  const numUsers = parseInt(inner);
-
-  console.log('numUsers: ', numUsers);
-
-  /**
-   * GitHub shows 10 resuls per page, so
-   */
-  return Math.ceil(numUsers / 10);
-}
-
-function upsertUser(userObj) {
-  const DB_URL = 'mongodb://localhost/thal';
-
-  if (mongoose.connection.readyState == 0) {
-    mongoose.connect(DB_URL);
-  }
-
-  // if this email exists, update the entry, don't insert
-  const conditions = { email: userObj.email };
-  const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-
-  User.findOneAndUpdate(conditions, userObj, options, (err, result) => {
-    if (err) {
-      throw err;
-    }
-  });
-}
-
+  await browser.close();
+};
+ 
 run();
